1c2fe50503732b4e54b5efcacce55b9ae127a142
dek-haji <deeeqhaji@gmail.com>
2019-05-22 13:26:56 -0500

    did kennel 6

--- src/component/animal/AnimalManager.js
+++ src/component/animal/AnimalManager.js
@@ -1,18 +1,31 @@
 const remoteURL = "http://localhost:5002"
+// const animalURL = "http://localhost:5002/animals"
 
-export default {
-    get(id) {
-        return fetch(`${remoteURL}/animals/${id}`).then(e => e.json())
+
+export default Object.create(null, {
+    get: {
+        value: function (id) {
+            /*
+                Since the purpose of this module is to be used by
+                all of the more specialized one, then the string
+                of `animals` should not be hard coded here.
+            */
+            return fetch(`${remoteURL}/animals/${id}`).then(e => e.json())
+        }
     },
-    getAll() {
-        return fetch(`${remoteURL}/animals`).then(e => e.json())
+    all: {
+        value: function () {
+            return fetch(`${remoteURL}/animals`).then(e => e.json())
+        }
     },
-    deleteAnimal(id) {
-        return fetch(`${remoteURL}/animals/${id}`, {
-            method: "DELETE",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }).then(e => e.json())
+    deleteAnimal: {
+        value: function (id) {
+            return fetch(`${remoteURL}/animals/${id}`, {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }).then(e => e.json())
+        }
     }
-}
+})
